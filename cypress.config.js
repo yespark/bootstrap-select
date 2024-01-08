af427a4a92947683afbdfa8c2eca0e7f672cc7cc
Navid Emad <navid.emad@yespark.fr>
2024-01-08 22:24:27 +0100

    fix virtual scroll breaks on last chunk

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,9 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   fixturesFolder: false,
   e2e: {
+    // Enables the "Run All Specs" UI feature, allowing the execution of
+    // multiple specs sequentially.
+    experimentalRunAllSpecs: true,
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
